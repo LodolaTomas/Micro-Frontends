4eb10ea4218ded4cacbcfb1f48326285c5f0573b
LodolaTomas <tomaslodola1@gmail.com>
2022-04-11 15:09:59 -0300

    microFrontents with angular

--- container/src/weincode-root-config.ts
+++ container/src/weincode-root-config.ts
@@ -7,6 +7,17 @@ registerApplication({
   activeWhen: ["/"]
 });
 
+registerApplication({
+  name: "@weincode/mf-main",
+  app: () => System.import("@weincode/mf-main"),
+  activeWhen: ["/"]
+});
+registerApplication({
+  name: "@weincode/mf-images",
+  app: () => System.import("@weincode/mf-images"),
+  activeWhen: ["/"]
+});
+
 start({
   urlRerouteOnly: true,
 });
--- screens/mf-images/src/app/app.component.ts
+++ screens/mf-images/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mf-header',
+  selector: 'mf-images',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mf-header';
+  title = 'mf-images';
 }
